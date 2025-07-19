fddce59889b6f9cf8325498ff1191494f03f2564
MRCAUSALIDAD <mrcausalidad@gmail.com>
2025-07-19 16:41:50 +0200

    add basic structure ide

--- website/ide-component.js
+++ website/ide-component.js
@@ -3,14 +3,23 @@
 class IDEComponente extends HTMLElement {
 
     connectedCallback(){
-        const content = this.innerHTML;
 
         this.innerHTML = `
-        <!-- HTML and styles -->
-        ${content}
+        <frame-component>
+            <div class="ide-frame">
+                <div class="ide-content-code">
+                    content code
+                </div>
+                <div class="ide-content-files">
+                    tree list files and folders...
+                </div>
+            </div>
+        </frame-component>
 
         `
     }
 
 
 }
+
+customElements.define('ide-component', IDEComponente);
