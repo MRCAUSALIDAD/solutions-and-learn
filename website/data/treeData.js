b103735a6150743d6be78bae7d209d40dba55dbe
MRCAUSALIDAD <mrcausalidad@gmail.com>
2025-07-20 11:34:20 +0200

    some changes of design and add folders in tree

--- website/data/treeData.js
+++ website/data/treeData.js
@@ -1,12 +1,63 @@
 export const treeData = [
     { label: 'README.md', path: 'files/README.md' },
     { label: 'math', children: [
-        { label: 'prueba1.tex', path: 'files/math/prueba1.tex' }
+        {label:'Basic Arithmetic', children:[{ label: 'prueba1.tex', path: 'files/math/prueba1.tex' }]},
+        {label:'Number Theory (Basics)', children:[{label:'', path:''}]},
+        {label:'Algebra (Fundamentals)', children:[{label:'', path:''}]},
+        {label:'Geometry (Basics)', children:[{label:'', path:''}]},
+        {label:'Coordinate Geometry', children:[{label:'', path:''}]},
+        {label:'Intermediate Algebra', children:[{label:'', path:''}]},
+        {label:'Functions', children:[{label:'', path:''}]},
+        {label:'Trigonometry', children:[{label:'', path:''}]},
+        {label:'Advanced Geometry', children:[{label:'', path:''}]},
+        {label:'Statistics and Probability', children:[{label:'', path:''}]},
+        {label:'Pre-Calculus', children:[{label:'', path:''}]},
+        {label:'Calculus', children:[{label:'', path:''}]},
+        {label:'Linear Algebra', children:[{label:'', path:''}]},
+        {label:'Discrete Mathematics', children:[{label:'', path:''}]},
+        {label:'Differential Equations', children:[{label:'', path:''}]},
     ]},
     { label: 'physics', children: [
-        { label: 'prueba1.tex', path: 'files/physics/prueba1.tex' }
+        { label: 'Basic Concepts: Quantities and Units', children: [
+        { label: 'prueba1.tex', path: 'files/physics/prueba1.tex' },
+        ]},
+        { label: 'Kinematics', children: [
+            { label: '', path: '' },
+        ]},
+        { label: 'Dynamics', children: [
+            { label: '', path: '' },
+        ]},
+        { label: 'Work, Energy, and Power', children: [
+            { label: '', path: '' },
+        ]},
+        { label: 'Momentum (Impulse and Momentum)', children: [
+            { label: '', path: '' },
+        ]},
+        { label: 'Gravitation', children: [
+            { label: '', path: '' },
+        ]},
+        { label: 'Statics', children: [
+            { label: '', path: '' },
+        ]},
+        { label: 'Oscillations and Waves', children: [
+            { label: '', path: '' },
+        ]},
+        { label: 'Thermodynamics', children: [
+            { label: '', path: '' },
+        ]},
+        { label: 'Electricity and Magnetism', children: [
+            { label: '', path: '' },
+        ]},
+        { label: 'Optics', children: [
+            { label: '', path: '' },
+        ]},
+        { label: 'Modern Physics', children: [
+            { label: '', path: '' },
+        ]},
+
     ]},
-    { label: 'programming', children: [
-        { label: 'prueba1.cpp', path: 'files/programming/prueba1.cpp' }
+    { label: 'programming', children: [,
+        {label:'', children:[{ label: 'prueba1.cpp', path: 'files/programming/prueba1.cpp' }]},
+        {label:'', children:[{label:'', path:''}]}
     ]}
 ];
