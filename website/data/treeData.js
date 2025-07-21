c0eadf0583068281d910b3c71d131e033bf8417b
MRCAUSALIDAD <mrcausalidad@gmail.com>
2025-07-21 10:29:55 +0200

    add var title and link

--- website/data/treeData.js
+++ website/data/treeData.js
@@ -1,65 +1,65 @@
 export const treeData = [
     { label: 'README.md', path: 'files/README.md' },
     { label: 'math', children: [
-        {label:'Basic Arithmetic', children:[{ label: 'prueba1.tex', path: 'files/math/prueba1.tex' }]},
-        {label:'Number Theory (Basics)', children:[{label:'', path:''}]},
-        {label:'Algebra (Fundamentals)', children:[{label:'', path:''}]},
-        {label:'Geometry (Basics)', children:[{label:'', path:''}]},
-        {label:'Coordinate Geometry', children:[{label:'', path:''}]},
-        {label:'Intermediate Algebra', children:[{label:'', path:''}]},
-        {label:'Functions', children:[{label:'', path:''}]},
-        {label:'Trigonometry', children:[{label:'', path:''}]},
-        {label:'Advanced Geometry', children:[{label:'', path:''}]},
-        {label:'Statistics and Probability', children:[{label:'', path:''}]},
-        {label:'Pre-Calculus', children:[{label:'', path:''}]},
-        {label:'Calculus', children:[{label:'', path:''}]},
-        {label:'Linear Algebra', children:[{label:'', path:''}]},
-        {label:'Discrete Mathematics', children:[{label:'', path:''}]},
-        {label:'Differential Equations', children:[{label:'', path:''}]},
+        {label:'Basic Arithmetic', children:[{ label: 'prueba1.tex', path: 'files/math/prueba1.tex', title:'Exercise arith', link:'https://www.google.es'}]},
+        {label:'Number Theory (Basics)', children:[{label:'', path:'', title:'', link:''}]},
+        {label:'Algebra (Fundamentals)', children:[{label:'', path:'', title:'', link:''}]},
+        {label:'Geometry (Basics)', children:[{label:'', path:'', title:'', link:''}]},
+        {label:'Coordinate Geometry', children:[{label:'', path:'', title:'', link:''}]},
+        {label:'Intermediate Algebra', children:[{label:'', path:'', title:'', link:''}]},
+        {label:'Functions', children:[{label:'', path:'', title:'', link:''}]},
+        {label:'Trigonometry', children:[{label:'', path:'', title:'', link:''}]},
+        {label:'Advanced Geometry', children:[{label:'', path:'', title:'', link:''}]},
+        {label:'Statistics and Probability', children:[{label:'', path:'', title:'', link:''}]},
+        {label:'Pre-Calculus', children:[{label:'', path:'', title:'', link:''}]},
+        {label:'Calculus', children:[{label:'', path:'', title:'', link:''}]},
+        {label:'Linear Algebra', children:[{label:'', path:'', title:'', link:''}]},
+        {label:'Discrete Mathematics', children:[{label:'', path:'', title:'', link:''}]},
+        {label:'Differential Equations', children:[{label:'', path:'', title:'', link:''}]},
     ]},
     { label: 'physics', children: [
         { label: 'Basic Concepts: Quantities and Units', children: [
-        { label: 'prueba1.tex', path: 'files/physics/prueba1.tex' },
+        { label: 'prueba1.tex', path: 'files/physics/prueba1.tex', title:'Exercise concept', link:'https://www.google.es' },
         ]},
         { label: 'Kinematics', children: [
-            { label: '', path: '' },
+            { label: '', path: '', title:'', link:'' },
         ]},
         { label: 'Dynamics', children: [
-            { label: '', path: '' },
+            { label: '', path: '', title:'', link:'' },
         ]},
         { label: 'Work, Energy, and Power', children: [
-            { label: '', path: '' },
+            { label: '', path: '', title:'', link:'' },
         ]},
         { label: 'Momentum (Impulse and Momentum)', children: [
-            { label: '', path: '' },
+            { label: '', path: '', title:'', link:'' },
         ]},
         { label: 'Gravitation', children: [
-            { label: '', path: '' },
+            { label: '', path: '', title:'', link:'' },
         ]},
         { label: 'Statics', children: [
-            { label: '', path: '' },
+            { label: '', path: '', title:'', link:'' },
         ]},
         { label: 'Oscillations and Waves', children: [
-            { label: '', path: '' },
+            { label: '', path: '', title:'', link:'' },
         ]},
         { label: 'Thermodynamics', children: [
-            { label: '', path: '' },
+            { label: '', path: '', title:'', link:'' },
         ]},
         { label: 'Electricity and Magnetism', children: [
-            { label: '', path: '' },
+            { label: '', path: '', title:'', link:'' },
         ]},
         { label: 'Optics', children: [
-            { label: '', path: '' },
+            { label: '', path: '', title:'', link:'' },
         ]},
         { label: 'Modern Physics', children: [
-            { label: '', path: '' },
+            { label: '', path: '', title:'', link:'' },
         ]},
 
     ]},
     { label: 'programming', children: [,
         {label:'C++', children:[{ label: 'prueba1.cpp', path: 'files/programming/prueba1.cpp' }]},
-        {label:'Python', children:[{label:'', path:''}]},
-        {label:'JavaScript', children:[{label:'', path:''}]},
-        {label:'Java', children:[{label:'', path:''}]},
+        {label:'Python', children:[{label:'', path:'', title:'', link:''}]},
+        {label:'JavaScript', children:[{label:'', path:'', title:'', link:''}]},
+        {label:'Java', children:[{label:'', path:'', title:'', link:''}]},
     ]}
 ];
