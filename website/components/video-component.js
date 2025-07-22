d73a9d54e216095c763dadd7138f533192d58d55
MRCAUSALIDAD <mrcausalidad@gmail.com>
2025-07-22 08:02:19 +0200

    carrousel videos

--- website/components/video-component.js
+++ website/components/video-component.js
@@ -1,29 +1,57 @@
-import { treeData } from "../data/treeData.js";
-
 class VideoComponent extends HTMLElement {
 
+    videos = [1,2,3,4,5,6,7,8,9,10,11];
+
     connectedCallback(){
-        const content = this.innerHTML;
         this.innerHTML = `
             <div>
-            <p>List of videos</p>
-            <tree-component id="videoTree"></tree-component>
+                <div class="carrousel-wrapper">
+                    <button class="btn-arrow left">&#9664;</button>
+                    <div class="carrousel-track">
+                        <div class="carrousel-track-inner"></div>
+                    </div>
+                    <button class="btn-arrow right">&#9654;</button>
+                </div>
             </div>
         `;
 
-         Promise.resolve().then(() => {
-            const tree = this.querySelector('#videoTree');
-            if (tree) {
-                tree.data = treeData; //<-- TODO: Change for link of videos
+        Promise.resolve().then(() => {
+            const trackInner = this.querySelector('.carrousel-track-inner');
+            const btnLeft = this.querySelector('.btn-arrow.left');
+            const btnRight = this.querySelector('.btn-arrow.right');
 
-                tree.addEventListener('file-selected', (e) => {
-                    const content = e.detail.content;
-                    console.log(content)
-                });
-            }
+            const cardWidth = 120 + 10;  // ancho tarjeta + gap
+
+            const videos = this.videos.map((v) => {
+                const video = document.createElement('div');
+                video.className = 'content-video';
+                video.style.backgroundImage = `url('https://via.placeholder.com/120x180?text=Video+${v}')`;
+                return video;
+            });
+
+            trackInner.append(...videos);
+
+            let scrollX = 0;
+
+            const scrollMax = () => {
+                return trackInner.scrollWidth - trackInner.parentElement.clientWidth;
+            };
+
+            const updateScroll = () => {
+                trackInner.style.transform = `translateX(${-scrollX}px)`;
+            };
+
+            btnLeft.addEventListener('click', () => {
+                scrollX = Math.max(0, scrollX - cardWidth * 3);
+                updateScroll();
+            });
+
+            btnRight.addEventListener('click', () => {
+                scrollX = Math.min(scrollMax(), scrollX + cardWidth * 3);
+                updateScroll();
+            });
         });
     }
-
 }
 
 customElements.define('video-component', VideoComponent);
--- website/data/treeData.js
+++ website/data/treeData.js
@@ -1,65 +1,65 @@
 export const treeData = [
     { label: 'README.md', path: 'files/README.md' },
     { label: 'math', children: [
-        {label:'Basic Arithmetic', children:[{ label: 'prueba1.tex', path: 'files/math/prueba1.tex', title:'Exercise arith', link:'https://www.google.es'}]},
-        {label:'Number Theory (Basics)', children:[{label:'', path:'', title:'', link:''}]},
-        {label:'Algebra (Fundamentals)', children:[{label:'', path:'', title:'', link:''}]},
-        {label:'Geometry (Basics)', children:[{label:'', path:'', title:'', link:''}]},
-        {label:'Coordinate Geometry', children:[{label:'', path:'', title:'', link:''}]},
-        {label:'Intermediate Algebra', children:[{label:'', path:'', title:'', link:''}]},
-        {label:'Functions', children:[{label:'', path:'', title:'', link:''}]},
-        {label:'Trigonometry', children:[{label:'', path:'', title:'', link:''}]},
-        {label:'Advanced Geometry', children:[{label:'', path:'', title:'', link:''}]},
-        {label:'Statistics and Probability', children:[{label:'', path:'', title:'', link:''}]},
-        {label:'Pre-Calculus', children:[{label:'', path:'', title:'', link:''}]},
-        {label:'Calculus', children:[{label:'', path:'', title:'', link:''}]},
-        {label:'Linear Algebra', children:[{label:'', path:'', title:'', link:''}]},
-        {label:'Discrete Mathematics', children:[{label:'', path:'', title:'', link:''}]},
-        {label:'Differential Equations', children:[{label:'', path:'', title:'', link:''}]},
+        {label:'Basic Arithmetic', children:[{ label: 'prueba1.tex', path: 'files/math/prueba1.tex'}]},
+        {label:'Number Theory (Basics)', children:[{label:'', path:''}]},
+        {label:'Algebra (Fundamentals)', children:[{label:'', path:''}]},
+        {label:'Geometry (Basics)', children:[{label:'', path:''}]},
+        {label:'Coordinate Geometry', children:[{label:'', path:''}]},
+        {label:'Intermediate Algebra', children:[{label:'', path:''}]},
+        {label:'Functions', children:[{label:'', path:''}]},
+        {label:'Trigonometry', children:[{label:'', path:''}]},
+        {label:'Advanced Geometry', children:[{label:'', path:''}]},
+        {label:'Statistics and Probability', children:[{label:'', path:''}]},
+        {label:'Pre-Calculus', children:[{label:'', path:''}]},
+        {label:'Calculus', children:[{label:'', path:''}]},
+        {label:'Linear Algebra', children:[{label:'', path:''}]},
+        {label:'Discrete Mathematics', children:[{label:'', path:''}]},
+        {label:'Differential Equations', children:[{label:'', path:''}]},
     ]},
     { label: 'physics', children: [
         { label: 'Basic Concepts: Quantities and Units', children: [
-        { label: 'prueba1.tex', path: 'files/physics/prueba1.tex', title:'Exercise concept', link:'https://www.google.es' },
+        { label: 'prueba1.tex', path: 'files/physics/prueba1.tex' },
         ]},
         { label: 'Kinematics', children: [
-            { label: '', path: '', title:'', link:'' },
+            { label: '', path: '' },
         ]},
         { label: 'Dynamics', children: [
-            { label: '', path: '', title:'', link:'' },
+            { label: '', path: '' },
         ]},
         { label: 'Work, Energy, and Power', children: [
-            { label: '', path: '', title:'', link:'' },
+            { label: '', path: '' },
         ]},
         { label: 'Momentum (Impulse and Momentum)', children: [
-            { label: '', path: '', title:'', link:'' },
+            { label: '', path: '' },
         ]},
         { label: 'Gravitation', children: [
-            { label: '', path: '', title:'', link:'' },
+            { label: '', path: '' },
         ]},
         { label: 'Statics', children: [
-            { label: '', path: '', title:'', link:'' },
+            { label: '', path: '' },
         ]},
         { label: 'Oscillations and Waves', children: [
-            { label: '', path: '', title:'', link:'' },
+            { label: '', path: '' },
         ]},
         { label: 'Thermodynamics', children: [
-            { label: '', path: '', title:'', link:'' },
+            { label: '', path: '' },
         ]},
         { label: 'Electricity and Magnetism', children: [
-            { label: '', path: '', title:'', link:'' },
+            { label: '', path: '' },
         ]},
         { label: 'Optics', children: [
-            { label: '', path: '', title:'', link:'' },
+            { label: '', path: '' },
         ]},
         { label: 'Modern Physics', children: [
-            { label: '', path: '', title:'', link:'' },
+            { label: '', path: '' },
         ]},
 
     ]},
     { label: 'programming', children: [,
         {label:'C++', children:[{ label: 'prueba1.cpp', path: 'files/programming/prueba1.cpp' }]},
-        {label:'Python', children:[{label:'', path:'', title:'', link:''}]},
-        {label:'JavaScript', children:[{label:'', path:'', title:'', link:''}]},
-        {label:'Java', children:[{label:'', path:'', title:'', link:''}]},
+        {label:'Python', children:[{label:'', path:''}]},
+        {label:'JavaScript', children:[{label:'', path:''}]},
+        {label:'Java', children:[{label:'', path:''}]},
     ]}
 ];
